2d1d91b1b3d1f3bf87e61aead617b82c71f9205c
IrinaBBB <balkarovairina@gmail.com>
2021-04-01 08:11:29 +0200

    adding IndexPage and Controller. closes #8

--- pet-clinic-web/src/main/java/ru/aurorahost/springpetclinic/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/ru/aurorahost/springpetclinic/SpringPetClinicApplication.java
@@ -1,4 +1,4 @@
-package ru.aurorahost.springpetclinic.model.springpetclinic;
+package ru.aurorahost.springpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
