cfb47fb667e84db0497158197f176bcd3c05e86d
IrinaBBB <balkarovairina@gmail.com>
2021-04-01 07:05:36 +0200

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/ru/aurorahost/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ru/aurorahost/springpetclinic/services/VetService.java
@@ -1,16 +1,7 @@
 package ru.aurorahost.springpetclinic.services;
-
-import ru.aurorahost.springpetclinic.model.Pet;
 import ru.aurorahost.springpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
