756995615765737fcb971b326d23205bac3fde05
IrinaBBB <balkarovairina@gmail.com>
2021-04-01 07:41:50 +0200

    impl map based services. closes #6

--- pet-clinic-data/src/main/java/ru/aurorahost/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ru/aurorahost/springpetclinic/services/PetService.java
@@ -2,8 +2,6 @@ package ru.aurorahost.springpetclinic.services;
 
 import ru.aurorahost.springpetclinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long> {
 
 }
---
+++ pet-clinic-data/src/main/java/ru/aurorahost/springpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package ru.aurorahost.springpetclinic.services.map;
+
+import ru.aurorahost.springpetclinic.model.Owner;
+import ru.aurorahost.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long>
+        implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+}
